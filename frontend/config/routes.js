a6e44e3daa54b7e9e3baec47f81ba38d0adcbd65
AoDeThri <740950342@qq.com>
2021-05-28 09:19:59 +0800

    remove useless pages

--- frontend/config/routes.js
+++ frontend/config/routes.js
@@ -4,17 +4,6 @@ export default [
     component: '../layouts/BlankLayout',
     routes: [
       {
-        path: '/user',
-        component: '../layouts/UserLayout',
-        routes: [
-          {
-            name: 'login',
-            path: '/user/login',
-            component: './User/login',
-          },
-        ],
-      },
-      {
         path: '/',
         component: '../layouts/SecurityLayout',
         routes: [
@@ -26,34 +15,6 @@ export default [
               {
                 path: '/',
                 redirect: '/welcome',
-              },
-              {
-                path: '/welcome',
-                name: 'welcome',
-                icon: 'smile',
-                component: './Welcome',
-              },
-              {
-                path: '/admin',
-                name: 'admin',
-                icon: 'crown',
-                component: './Admin',
-                authority: ['admin'],
-                routes: [
-                  {
-                    path: '/admin/sub-page',
-                    name: 'sub-page',
-                    icon: 'smile',
-                    component: './Welcome',
-                    authority: ['admin'],
-                  },
-                ],
-              },
-              {
-                name: 'list.table-list',
-                icon: 'table',
-                path: '/list',
-                component: './TableList',
               },
               {
                 name: '图片搜索',
